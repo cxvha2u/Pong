9e1f22c6aa59f23ec41444459bb4b5cd0942671f
xk0fe <x.k0fe@yandex.ru>
2019-07-23 22:29:22 +0300

    0.7.2
    Walls now are adjusted by the screen size.

---
+++ Assets/Scripts/MoveWallsByScreenSize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWallsByScreenSize : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject wallLeft;
+    [SerializeField]
+    private GameObject wallRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wallLeft.transform.position = new Vector3(-Screen.width, transform.position.y, transform.position.z);
+        wallRight.transform.position = new Vector3(Screen.width, transform.position.y, transform.position.z);
+        //wallLeft.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(-Screen.width, transform.position.y, transform.position.z));
+        //wallRight.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, transform.position.y, transform.position.z));
+    }
+}
